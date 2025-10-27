46f9efdc9ba0f011039420ba6475ec9fedb08ce0
Vahe <ohanyanv703@gmail.com>
2025-10-27 19:36:55 +0400

    constants for page

---
+++ src/constants/data.js
@@ -0,0 +1,9 @@
+const companys = [
+  { id: 1, name: "Prisma Labs", img:  "../src/assets/companys/Adobe-Express-Logo-PNG.png"},
+  { id: 2, name: "", img: "../src/assets/companys/Adobe-Express-Logo-PNG.png" },
+  { id: 3, name: "", img: "../src/assets/companys/Adobe-Express-Logo-PNG.png" },
+  { id: 4, name: "", img: "../src/assets/companys/Adobe-Express-Logo-PNG.png" },
+  { id: 5, name: "", img: "../src/assets/companys/Adobe-Express-Logo-PNG.png" },
+];
+
+export { companys };
