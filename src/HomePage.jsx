f3db0fef3229cbffcb765cc1eecbe297a8af0402
Vahe <ohanyanv703@gmail.com>
2025-10-31 00:39:55 +0400

    added collabs and gen ai

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-import {Outlet} from "react-router-dom"
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Hero from "./pages/Hero";
+import Features from "./pages/Features";
+import Collabs from "./pages/Collabs";
+import GenAI from "./pages/GenAI";
+import Pricing from "./pages/Pricing";
+import HowToUse from "./pages/HowToUse";
+import Roadmap from "./pages/Roadmap";
 
 const HomePage = () => {
-
-}
+  return (
+    <React.Fragment>
+      <Hero />
+      <div className="bg-[#0E0C15]">
+        <Features />
+        <Collabs />
+        <GenAI/>
+        {/* <Pricing />  */}
+        <HowToUse />
+        <Roadmap />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default HomePage;
