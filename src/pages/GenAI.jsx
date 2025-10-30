0e4818907726e84fd56cc4e6d8b1cd038582bf63
Vahe <ohanyanv703@gmail.com>
2025-10-31 00:38:37 +0400

    gen ai part

---
+++ src/pages/GenAI.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GenAI = () => {
+    return (
+        <div className='relative mt-20'>
+            <div className='hidden absolute w-[0.2px] bg-[#252134] top-0 bottom-0 left-10  xl:block'></div>
+            <div className='hidden absolute w-[0.2px] bg-[#252134] top-0 bottom-0 right-10 xl:block'></div>
+            <h1 className='text-center text-6xl text-white'>Generative AI made for creators.</h1>
+        </div>
+    )
+}
+
+export default GenAI
