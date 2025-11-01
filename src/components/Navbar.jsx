a236204acd14433a3a34fe65f9f4d18c35c48c63
Vahe <ohanyanv703@gmail.com>
2025-11-01 20:05:35 +0400

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from "./Button";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../index.css";
-import { BottomLine } from "./BottomLine";
-
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
 const Navbar = () => {
   const location = useLocation();
@@ -20,82 +16,44 @@ const Navbar = () => {
   useEffect(() => {
     const sectionId = pathToId[location.pathname];
     if (sectionId) {
-       document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
-      // if (element) {
-      // }
+      document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
     }
-    console.log(location.pathname);
   }, [location.pathname]);
 
-  // Trigger when the path changes
-  // Get the current section ID from the map
-  // Call scrollIntoView()
-
-  // const scrolling = (path) => {
-  //   document.getElementById(path).scrollIntoView({ behavior: "smooth" });
-  // };
-
   return (
-    <React.Fragment>
-      <section id="navbar">
-        <div className="flex justify-between items-center fixed z-50 w-full pl-10 bg-[#0E0C15]/50 backdrop-blur-lg text-2xl p-6">
-          <div className="">
-            <Link to="">
-              <img src="src/assets/brainwave.svg" alt="" className="" />
-            </Link>
-          </div>
-          <div className="flex justify-between items-center text-[#CAC6DD] text-lg gap-20 font-mono transition-colors duration-500">
-            <Link
-              to="features" /*  */
-              // onClick={() => {
-              //   scrolling();
-              // }}
-              className=" hover:text-white"
-            >
-              FEATURES
-            </Link>
-            <Link
-              to="pricing"
-              // onClick={() => {
-              //   scrolling();
-              // }}
-              className=" hover:text-white"
-            >
-              PRICING
-            </Link>
-            <Link
-              to="how-to-use"
-              // onClick={() => {
-              //   scrolling();
-              // }}
-              className="hover:text-white"
-            >
-              HOW TO USE
-            </Link>
-            <Link
-              to="roadmap"
-              // onClick={() => {
-              //   scrolling();
-              // }}
-              className=" hover:text-white"
-            >
-              ROADMAP
-            </Link>
-          </div>
-          <div className="flex justify-between items-center pr-5 gap-10 text-lg font-mono text-[#CAC6DD]">
-            <Link to="signup" className="hover:text-white">
-              NEW ACCOUNT
-            </Link>
-            <Link to="login">
-              <Button className="cursor-pointer hover:text-white lg:flex">
-                SIGN IN
-              </Button>
-            </Link>
-          </div>
+    <section id="navbar">
+      <div className="flex justify-between items-center fixed z-50 w-full pl-10 bg-[#0E0C15]/50 backdrop-blur-lg text-2xl p-6">
+        <div className="">
+          <Link to="">
+            <img src="src/assets/brainwave.svg" alt="" className="" />
+          </Link>
         </div>
-        {/* <BottomLine/> */}
-      </section>
-    </React.Fragment>
+        <div className="flex justify-between items-center text-[#CAC6DD] text-sm gap-20 font-mono transition-colors duration-500">
+          <Link to="features" className=" hover:text-white">
+            FEATURES
+          </Link>
+          <Link to="pricing" className=" hover:text-white">
+            PRICING
+          </Link>
+          <Link to="how-to-use" className="hover:text-white">
+            HOW TO USE
+          </Link>
+          <Link to="roadmap" className=" hover:text-white">
+            ROADMAP
+          </Link>
+        </div>
+        <div className="flex justify-between items-center pr-5 gap-10 text-sm font-mono text-[#CAC6DD]">
+          <Link to="signup" className="hover:text-white">
+            NEW ACCOUNT
+          </Link>
+          <Link to="login">
+            <Button className="cursor-pointer hover:text-white lg:flex">
+              SIGN IN
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
