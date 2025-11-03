a91293fdeb9c2a55a0002b056b6bbd88f6ba9e21
Vahe <ohanyanv703@gmail.com>
2025-11-04 00:46:33 +0400

    Roadmap part gradients

---
+++ src/components/RoadmapGradient.jsx
@@ -0,0 +1,23 @@
+export const RoadmapGradient = () => {
+  return (
+    <div className="absolute top-10 w-200 ml-0 h-300 pointer-events-none opacity-22 z-0">
+      <img
+        src="src/assets/gradient.png"
+        alt="gradient"
+        className="absolute z-10"
+      />
+    </div>
+  );
+};
+
+export const RoadmapGradient2 = () => {
+  return (
+    <div className="absolute bottom-250 w-200 right-0 h-0 pointer-events-none opacity-22 z-0">
+      <img
+        src="src/assets/gradient.png"
+        alt="gradient"
+        className="absolute z-10"
+      />
+    </div>
+  );
+};
