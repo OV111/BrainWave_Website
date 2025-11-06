3ba658c8b8ff3e1f6935a18eb51678236948b86e
Vahe <ohanyanv703@gmail.com>
2025-11-06 21:41:14 +0400

    App js

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { lazy } from "react";
 import "./index.css";
-// import { RouterProvider } from 'react-router-dom'
-import { Suspense } from "react";
 import { HashRouter } from "react-router-dom";
-import LoadingSuspense from "./components/LoadingSuspense";
 import Navbar from "./components/Navbar";
 import ButtonGradient from "./components/ButtonGradient";
-const HomePage = lazy(() => import("./HomePage"));
+import HomePage from "./HomePage";
 
 function App() {
   return (
     <React.Fragment>
       <HashRouter>
         <Navbar />
-        <Suspense fallback={<LoadingSuspense />}>
-          <HomePage />
-        </Suspense>
+        <HomePage />
       </HashRouter>
       <ButtonGradient />
     </React.Fragment>
